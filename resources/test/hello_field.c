1cb2789249402588302f5e03a30207d702690fc6
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2024-09-24 15:47:21 +0100

    added more testcases

---
+++ resources/test/hello_field.c
@@ -0,0 +1,10 @@
+#include <assert.h>
+struct obj {
+  int a;
+  int b;
+};
+
+int main() {
+  struct obj qwe;
+  assert(qwe.a); 
+}
---
+++ resources/test/hello_mul.c
@@ -0,0 +1,4 @@
+#include <assert.h>
+int main() {
+  assert(10 * 2 < 20);
+}

621fafbdad730269c9e9e150c481c2fa8e96a4bf
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2024-10-26 19:54:04 +0100

    a bit of cleaning up

--- src/cbmc.rs
+++ src/cbmc.rs
@@ -1,13 +1,17 @@
 use std::collections::HashMap;
 
 use crate::ByteReader;
-use crate::ByteWriter;
 pub use crate::Irept;
 use log::debug;
 
+///////////////
+// CBMC DATA //
+///////////////
+
+// Direct parsing result of a symbol
 #[derive(Clone, Debug)]
 pub struct CBMCSymbol {
-    pub stype: Irept,
+    pub stype: Irept, // stype => type. Rust reserves some weird words
     pub value: Irept,
     pub location: Irept,
     pub name: String,
@@ -19,22 +23,189 @@ pub struct CBMCSymbol {
     pub is_type: bool,
 }
 
-impl Default for CBMCSymbol {
-    fn default() -> Self {
-        CBMCSymbol {
-            stype: Irept::default(),
-            value: Irept::default(),
-            location: Irept::default(),
-            name: String::default(),
-            module: String::default(),
-            base_name: String::default(),
-            mode: String::default(),
-            pretty_name: String::default(),
-            flags: 0,
-            is_type: false,
-        }
+impl CBMCSymbol {
+    fn fix_symbol(&mut self) {
+        
+        
     }
 }
+
+// Direct parsing result of an instruction
+#[derive(Clone, Debug)]
+pub struct CBMCInstruction {
+    pub code: Irept,
+    pub source_location: Irept,
+    pub instr_type: u32,
+    pub guard: Irept,
+    pub target_number: u32,
+    pub targets: Vec<Irept>,
+    pub labels: Vec<String>,
+    pub function: Irept,
+}
+
+// Direct parsing result of a function
+#[derive(Clone, Debug)]
+pub struct CBMCFunction {
+    pub name: String,
+    pub instructions: Vec<CBMCInstruction>,
+}
+
+impl CBMCFunction {
+    fn fix_name(name: &str) -> String {
+         match name {
+            "__CPROVER__start" => String::from("__ESBMC_main"),
+            _ => String::from(name)
+        }        
+    }
+}
+
+// The actual parser
+#[derive(Clone, Debug)]
+pub struct CBMCParser {
+    pub reader: ByteReader,
+    pub symbols_irep: Vec<Irept>,
+    pub functions_irep: Vec<(String, Irept)>,
+    pub struct_cache: HashMap<Irept, Irept>,
+}
+
+pub fn process_cbmc_file(path: &str) -> CBMCParser {
+    let mut result = CBMCParser {
+        reader: ByteReader::read_file(path),
+        functions_irep: Vec::new(),
+        symbols_irep: Vec::new(),
+        struct_cache: HashMap::new(),
+    };
+
+    result.reader.check_cbmc_header().unwrap();
+    result.reader.check_cbmc_version().unwrap();
+
+    // Symbol table
+    let number_of_symbols = result.reader.read_cbmc_word();
+    debug!("Got {} symbols", number_of_symbols);
+
+    for _ in 0..number_of_symbols {
+        let mut sym = CBMCSymbol::default();
+
+        // Type is a... type
+        sym.stype = result.reader.read_cbmc_reference();
+        // Value is an expr
+        sym.value = result.reader.read_cbmc_reference();
+        // Location is just a string
+        sym.location = result.reader.read_cbmc_reference();
+
+        // Name is just a string
+        sym.name = result.reader.read_cbmc_string_ref();
+        // Module is just a string
+        sym.module = result.reader.read_cbmc_string_ref();
+        // Base name is just a string
+        sym.base_name = result.reader.read_cbmc_string_ref();
+        // Symbol mode conveys the language (C, C++, Rust, etc)
+        sym.mode = result.reader.read_cbmc_string_ref();
+        // String
+        sym.pretty_name = result.reader.read_cbmc_string_ref();
+
+        // Ordering is used for historical reasons.
+        let ordering = result.reader.read_cbmc_word();
+        assert_eq!(ordering,0);
+        
+        sym.flags = result.reader.read_cbmc_word();
+
+        sym.is_type = sym.flags & (1 << 15) != 0;
+        if sym.is_type && sym.stype.id == "struct" {
+            // Type caching
+            sym.stype
+                .named_subt
+                .insert("tag".to_string(), Irept::from(&sym.base_name));
+            let tagname = Irept::from(format!("tag-{}", &sym.base_name));
+            sym.stype.fix_type(&result.struct_cache);
+            result.struct_cache.insert(tagname, sym.stype.clone());
+        }
+
+        // sym.is_weak = (flags &(1 << 16))!=0;
+        // sym.is_type = (flags &(1 << 15))!=0;
+        // sym.is_property = (flags &(1 << 14))!=0;
+        // sym.is_macro = (flags &(1 << 13))!=0;
+        // sym.is_exported = (flags &(1 << 12))!=0;
+        // // sym.is_input = (flags &(1 << 11))!=0;
+        // sym.is_output = (flags &(1 << 10))!=0;
+        // sym.is_state_var = (flags &(1 << 9))!=0;
+        // sym.is_parameter = (flags &(1 << 8))!=0;
+        // sym.is_auxiliary = (flags &(1 << 7))!=0;
+        // // sym.binding = (flags &(1 << 6))!=0;
+        // sym.is_lvalue = (flags &(1 << 5))!=0;
+        // sym.is_static_lifetime = (flags &(1 << 4))!=0;
+        // sym.is_thread_local = (flags &(1 << 3))!=0;
+        // sym.is_file_local = (flags &(1 << 2))!=0;
+        // sym.is_extern = (flags &(1 << 1))=0;
+        // sym.is_volatile = (flags &1)!=0;
+
+        result.symbols_irep.push(Irept::from(sym));
+    }
+
+    // Lets double check for fixes
+    for symbol in &mut result.symbols_irep {
+        symbol.fix_type(&result.struct_cache);
+        assert_ne!(symbol.named_subt["type"].id, "struct_tag");
+        assert_ne!(symbol.named_subt["type"].id, "c_bool");
+    }
+
+
+    // Functions
+    let number_of_functions = result.reader.read_cbmc_word();
+    debug!("Got {} functions", number_of_functions);
+    for _ in 0..number_of_functions {
+        let function_name = CBMCFunction::fix_name(&result.reader.read_gb_string());
+        let num_of_instructions =  result.reader.read_cbmc_word();
+
+        let mut function = CBMCFunction {
+            name: function_name,
+            instructions: Vec::with_capacity(num_of_instructions as usize),
+        };
+
+        for _ in 0..num_of_instructions {
+            // # instructions
+            let code = result.reader.read_cbmc_reference();
+
+            let source_location = result.reader.read_cbmc_reference();
+            let instr_type = result.reader.read_cbmc_word();
+            let guard = result.reader.read_cbmc_reference();
+            let target_number = result.reader.read_cbmc_word();
+
+            // Add targets
+            let t_count = result.reader.read_cbmc_word();
+            let mut targets: Vec<Irept> = Vec::new();
+            for _ in 0..t_count {
+                targets.push(Irept::from(result.reader.read_cbmc_word().to_string()));
+            }
+
+            // Add labels
+            let l_count = result.reader.read_cbmc_word();
+            let mut labels: Vec<String> = Vec::default();
+            for _ in 0..l_count {
+                let label = result.reader.read_cbmc_string_ref();
+                labels.push(label);
+            }
+
+            function.instructions.push(CBMCInstruction {
+                code,
+                source_location,
+                instr_type,
+                guard,
+                target_number,
+                targets,
+                labels,
+                function: Irept::from(&function.name),
+            })
+        }
+        let function_name = function.name.clone();
+        let mut function_irep = Irept::from(function);
+        function_irep.fix_type(&result.struct_cache);
+
+        result.functions_irep.push((function_name, function_irep));
+    }
+    result
+}
+
 
 impl From<CBMCSymbol> for Irept {
     fn from(data: CBMCSymbol) -> Self {
@@ -82,24 +253,6 @@ impl From<CBMCSymbol> for Irept {
         result.fix_expression();
         result
     }
-}
-
-#[derive(Clone, Debug)]
-pub struct CBMCInstruction {
-    pub code: Irept,
-    pub source_location: Irept,
-    pub instr_type: u32,
-    pub guard: Irept,
-    pub target_number: u32,
-    pub targets: Vec<Irept>,
-    pub labels: Vec<String>,
-    pub function: Irept,
-}
-
-#[derive(Clone, Debug)]
-pub struct CBMCFunction {
-    pub name: String,
-    pub instructions: Vec<CBMCInstruction>,
 }
 
 impl From<CBMCInstruction> for Irept {
@@ -162,13 +315,23 @@ impl From<CBMCFunction> for Irept {
     }
 }
 
-#[derive(Clone, Debug)]
-pub struct CBMCParser {
-    pub reader: ByteReader,
-    pub symbols_irep: Vec<Irept>,
-    pub functions_irep: Vec<(String, Irept)>,
-    pub struct_cache: HashMap<Irept, Irept>,
+impl Default for CBMCSymbol {
+    fn default() -> Self {
+        CBMCSymbol {
+            stype: Irept::default(),
+            value: Irept::default(),
+            location: Irept::default(),
+            name: String::default(),
+            module: String::default(),
+            base_name: String::default(),
+            mode: String::default(),
+            pretty_name: String::default(),
+            flags: 0,
+            is_type: false,
+        }
+    }
 }
+
 
 #[derive(Clone, Debug)]
 enum Component {
@@ -185,6 +348,10 @@ enum Component {
     Pointer {
         to: Box<Component>,
     },
+}
+
+impl Component {
+    
 }
 
 fn from_struct(components: Vec<(String, Component)>) -> Irept {
@@ -363,10 +530,6 @@ impl Anon2Struct {
     }
 }
 
-impl Component {
-    // impl From<&str> for Component {
-}
-
 impl Irept {
     pub fn expand_anon_struct(&mut self) {
         if self.named_subt.contains_key("components") {
@@ -462,147 +625,9 @@ impl Irept {
     }
 }
 
-pub fn process_cbmc_file(path: &str) -> CBMCParser {
-    let mut result = CBMCParser {
-        reader: ByteReader::read_file(path),
-        functions_irep: Vec::new(),
-        symbols_irep: Vec::new(),
-        struct_cache: HashMap::new(),
-    };
-
-    result.reader.check_cbmc_header().unwrap();
-    result.reader.check_cbmc_version().unwrap();
-
-    // Symbol table
-    let number_of_symbols = result.reader.read_cbmc_word();
-    debug!("Got {} symbols", number_of_symbols);
-
-    for _ in 0..number_of_symbols {
-        let mut sym = CBMCSymbol::default();
-        sym.stype = result.reader.read_cbmc_reference();
-        sym.value = result.reader.read_cbmc_reference();
-        sym.location = result.reader.read_cbmc_reference();
-
-        sym.name = result.reader.read_cbmc_string_ref();
-        sym.module = result.reader.read_cbmc_string_ref();
-        sym.base_name = result.reader.read_cbmc_string_ref();
-        sym.mode = result.reader.read_cbmc_string_ref();
-        sym.pretty_name = result.reader.read_cbmc_string_ref();
-
-        let ordering = result.reader.read_cbmc_word();
-
-        sym.flags = result.reader.read_cbmc_word();
-
-        sym.is_type = sym.flags & (1 << 15) != 0;
-        if sym.is_type && sym.stype.id == "struct" {
-            // Type caching
-            sym.stype
-                .named_subt
-                .insert("tag".to_string(), Irept::from(&sym.base_name));
-            let tagname = Irept::from(format!("tag-{}", &sym.base_name));
-            sym.stype.fix_type(&result.struct_cache);
-            result.struct_cache.insert(tagname, sym.stype.clone());
-        }
-
-        // sym.is_weak = (flags &(1 << 16))!=0;
-        // sym.is_type = (flags &(1 << 15))!=0;
-        // sym.is_property = (flags &(1 << 14))!=0;
-        // sym.is_macro = (flags &(1 << 13))!=0;
-        // sym.is_exported = (flags &(1 << 12))!=0;
-        // // sym.is_input = (flags &(1 << 11))!=0;
-        // sym.is_output = (flags &(1 << 10))!=0;
-        // sym.is_state_var = (flags &(1 << 9))!=0;
-        // sym.is_parameter = (flags &(1 << 8))!=0;
-        // sym.is_auxiliary = (flags &(1 << 7))!=0;
-        // // sym.binding = (flags &(1 << 6))!=0;
-        // sym.is_lvalue = (flags &(1 << 5))!=0;
-        // sym.is_static_lifetime = (flags &(1 << 4))!=0;
-        // sym.is_thread_local = (flags &(1 << 3))!=0;
-        // sym.is_file_local = (flags &(1 << 2))!=0;
-        // sym.is_extern = (flags &(1 << 1))=0;
-        // sym.is_volatile = (flags &1)!=0;
-
-        assert_eq!(ordering, 0);
-        //        sym.mode = "C".to_string();
-
-        if sym.mode != "C".to_string() {
-            println!("{}", Irept::from(sym.clone()));
-            sym.mode = "C".to_string();
-            sym.base_name = sym.name.clone();
-        }
-        let mut symbol_irep = Irept::from(sym);
-        symbol_irep.fix_type(&result.struct_cache);
-        result.symbols_irep.push(symbol_irep);
-    }
-
-    // Lets double check for fixes
-    for symbol in &mut result.symbols_irep {
-        symbol.fix_type(&result.struct_cache);
-        assert_ne!(symbol.named_subt["type"].id, "struct_tag");
-        assert_ne!(symbol.named_subt["type"].id, "c_bool");
-    }
-
-
-    // Functions
-    let number_of_functions = result.reader.read_cbmc_word();
-    debug!("Got {} functions", number_of_functions);
-    for _ in 0..number_of_functions {
-        let mut function_name = result.reader.read_gb_string();
-        if function_name == "__CPROVER__start" {
-            function_name = "__ESBMC_main".to_string();
-        }
-        let mut function = CBMCFunction {
-            name: function_name,
-            instructions: Vec::new(),
-        };
-
-        for _ in 0..result.reader.read_cbmc_word() {
-            // # instructions
-            let code = result.reader.read_cbmc_reference();
-
-            let source_location = result.reader.read_cbmc_reference();
-            let instr_type = result.reader.read_cbmc_word();
-            let guard = result.reader.read_cbmc_reference();
-            let target_number = result.reader.read_cbmc_word();
-
-            // Add targets
-            let t_count = result.reader.read_cbmc_word();
-            let mut targets: Vec<Irept> = Vec::new();
-            for _ in 0..t_count {
-                targets.push(Irept::from(result.reader.read_cbmc_word().to_string()));
-            }
-
-            // Add labels
-            let l_count = result.reader.read_cbmc_word();
-            let mut labels: Vec<String> = Vec::default();
-            for _ in 0..l_count {
-                let label = result.reader.read_cbmc_string_ref();
-                labels.push(label);
-            }
-
-            function.instructions.push(CBMCInstruction {
-                code,
-                source_location,
-                instr_type,
-                guard,
-                target_number,
-                targets,
-                labels,
-                function: Irept::from(&function.name),
-            })
-        }
-        let function_name = function.name.clone();
-        let mut function_irep = Irept::from(function);
-        function_irep.fix_type(&result.struct_cache);
-
-        result.functions_irep.push((function_name, function_irep));
-    }
-    result
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::ByteWriter;
     #[test]
     fn test_cbmc_to_esbmc_file() {
         let cargo_dir = match std::env::var("CARGO_MANIFEST_DIR") {

c2592fbd276e19ba249b7e835a8bb92f4f2049cd
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2024-10-26 19:18:38 +0100

    moved cbmc hacks to the bottom of the file
    Just while I organize CBMC.rs, eventually
    everything will be moved to the cbmc module

--- src/irep.rs
+++ src/irep.rs
@@ -1,5 +1,8 @@
+use json::object;
+use json::JsonValue;
 use std::collections::HashMap;
 use std::collections::HashSet;
+
 #[derive(Clone, Debug)]
 pub struct Irept {
     pub id: String,
@@ -9,71 +12,11 @@ pub struct Irept {
 }
 
 impl Irept {
-    pub fn fix_expression(&mut self) {
-        if self.id == "side_effect" {
-            self.id = "sideeffect".to_string();
-        }
-
-        if self.id == "constant" && self.named_subt.contains_key("#base") {
-            // Value ID might be the decimal/hexa representation, we want the binary one!
-            let number = u64::from_str_radix(&self.named_subt["value"].id, 16).unwrap();
-            self.named_subt.insert(
-                String::from("value"),
-                Irept::from(format!("{:064b}", number)),
-            );
-        }
-
-        let expressions: HashSet<String> = HashSet::from(
-            [
-                "member",
-                "typecast",
-                "notequal",
-                "or",
-                "mod",
-                "not",
-                "*",
-                "/",
-                "+",
-                "-",
-                "=",
-                "<",
-                "lshr",
-                "shl",
-                "address_of",
-                "index",
-                "pointer_object",
-                "array_of",
-                "sideeffect",
-                "dereference",
-                "bitand",
-            ]
-            .map(|x| x.to_string()),
-        );
-
-        if expressions.contains(&self.id) {
-            let mut operands = Irept::default();
-            operands.subt = self.subt.clone();
-            self.named_subt.insert("operands".to_string(), operands);
-            self.subt.clear();
-        }
-
-        for sub in &mut self.subt {
-            sub.fix_expression();
-        }
-
-        for (k, v) in &mut self.named_subt {
-            if k == "components" {
-                for sub in &mut v.subt {
-                    sub.id = "component".to_string();
-                }
-            }
-            v.fix_expression();
-        }
+    pub fn get_nil() -> Self {
+        Irept::from("nil")
     }
 }
 
-use json::object;
-use json::JsonValue;
 impl From<&Irept> for JsonValue {
     fn from(data: &Irept) -> Self {
         let mut obj = object! {id: data.id.clone()};
@@ -93,7 +36,6 @@ impl From<&Irept> for JsonValue {
         for (k, v) in &data.comments {
             obj[k] = JsonValue::from(v);
         }
-
         obj
     }
 }
@@ -168,7 +110,65 @@ impl From<&str> for Irept {
 }
 
 impl Irept {
-    pub fn get_nil() -> Self {
-        Irept::from("nil")
+    pub fn fix_expression(&mut self) {
+        if self.id == "side_effect" {
+            self.id = "sideeffect".to_string();
+        }
+
+        if self.id == "constant" && self.named_subt.contains_key("#base") {
+            // Value ID might be the decimal/hexa representation, we want the binary one!
+            let number = u64::from_str_radix(&self.named_subt["value"].id, 16).unwrap();
+            self.named_subt.insert(
+                String::from("value"),
+                Irept::from(format!("{:064b}", number)),
+            );
+        }
+
+        let expressions: HashSet<String> = HashSet::from(
+            [
+                "member",
+                "typecast",
+                "notequal",
+                "or",
+                "mod",
+                "not",
+                "*",
+                "/",
+                "+",
+                "-",
+                "=",
+                "<",
+                "lshr",
+                "shl",
+                "address_of",
+                "index",
+                "pointer_object",
+                "array_of",
+                "sideeffect",
+                "dereference",
+                "bitand",
+            ]
+            .map(|x| x.to_string()),
+        );
+
+        if expressions.contains(&self.id) {
+            let mut operands = Irept::default();
+            operands.subt = self.subt.clone();
+            self.named_subt.insert("operands".to_string(), operands);
+            self.subt.clear();
+        }
+
+        for sub in &mut self.subt {
+            sub.fix_expression();
+        }
+
+        for (k, v) in &mut self.named_subt {
+            if k == "components" {
+                for sub in &mut v.subt {
+                    sub.id = "component".to_string();
+                }
+            }
+            v.fix_expression();
+        }
     }
 }

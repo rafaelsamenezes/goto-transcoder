18d290dc7d847c8b0c70e3629fc18f7550f36fa3
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2024-11-19 08:02:00 +0000

    removed .dir-locals.el

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-((nil . ((eval . (progn
-                   (let ((my-project-file (expand-file-name "goto-transcoder-mode.el" (locate-dominating-file default-directory ".dir-locals.el"))))
-                     (when (file-exists-p my-project-file)
-                       (load my-project-file)
-                       (goto-transcoder-mode 1))))))))
